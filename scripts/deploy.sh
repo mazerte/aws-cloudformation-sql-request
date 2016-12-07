614a51fdb1723e95ec93b11906829b694612b9af
Mathieu Desvé <mathieu@ogury.co>
2016-12-07 13:25:44 +0100

    Add postgres engine

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,10 @@
 node_modules/.bin/cfn-lambda zip --output deploy/archive.zip
 
 echo "Deploy $TRAVIS_TAG version to S3"
-aws s3 cp deploy/archive.zip s3://chatanoo-deployment/aws-cloudformation-sql-request/$TRAVIS_TAG.zip
+aws s3 cp deploy/archive.zip s3://chatanoo-deployments-eu-west-1/aws-cloudformation-sql-request/$TRAVIS_TAG.zip
 
 echo "Upload latest"
 aws s3api put-object \
-  --bucket chatanoo-deployment \
+  --bucket chatanoo-deployments-eu-west-1 \
   --key aws-cloudformation-sql-request/latest.zip \
-  --website-redirect-location /chatanoo-deployment/aws-cloudformation-sql-request/$TRAVIS_TAG.zip
+  --website-redirect-location /chatanoo-deployments-eu-west-1/aws-cloudformation-sql-request/$TRAVIS_TAG.zip
